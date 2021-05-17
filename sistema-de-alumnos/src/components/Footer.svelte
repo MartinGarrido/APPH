857e47e0294c4a804041bdd2a72f65ff97fb6146
Martin Garrido <martingarrido95@gmail.com>
2021-05-17 13:10:42 -0300

    se crea la app de svelte

---
+++ sistema-de-alumnos/src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<style>
+    footer{
+        color: #bbb;
+        text-align: center;
+        padding:20px;
+        max-width: 400px;
+        margin: 40px auto 0;
+        border-top: 1px solid #fafafa;
+    }
+</style>
+<footer>copyright 2021 preto</footer>
